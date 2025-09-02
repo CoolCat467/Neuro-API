87a40b9e46390f101b154c8c44af64a4371088e2
CoolCat467 <52022020+CoolCat467@users.noreply.github.com>
2025-09-02 17:34:46 -0500

    Start adding readthedocs

---
+++ docs/source/api.rst
@@ -0,0 +1,19 @@
+API Reference
+=============
+
+.. automodule:: neuro_api
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+.. automodule:: neuro_api.api
+   :members:
+
+.. automodule:: neuro_api.command
+   :members:
+
+.. automodule:: neuro_api.event
+   :members:
+
+.. automodule:: neuro_api.trio_ws
+   :members:
---
+++ docs/source/conf.py
@@ -0,0 +1,36 @@
+"""Sphinx Configuration."""
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../src"))  # Path to your package
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "Neuro-API"
+copyright = "2025, CoolCat467"  # noqa: A001
+author = "CoolCat467"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",  # Auto-generate docs from docstrings
+    "sphinx.ext.napoleon",  # Support Google/NumPy docstring styles
+]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
+html_static_path = ["_static"]
